3a760aefc7bf814a8a61e3e94bf2227104dc2463
Elias Taveras <eliastave12@gmail.com>
2020-03-27 04:12:06 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,43 +1,22 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require_relative './directors_database'
+require 'directors_database'
 
-def directors_totals(source)
+def gross_for_director(director_data)
+  integer=0
+  i = 0
+  while i < director_data[:movies].length do
+  integer += director_data[:movies][i][:worldwide_gross]
+  i += 1
+  end
+ integer
+end
+ 
+def directors_totals(nds)
   result = {}
-  director_index = 0
-  while director_index < source.size do
-    director = source[director_index]
-    result[director[:name]] = gross_for_director(director)
-    director_index += 1
+  director_i = 0
+  while director_i < nds.length do
+    result[nds[director_i][:name]] = gross_for_director(directors_database[director_i])
+    director_i += 1
   end
-  result
+ result
 end
-
-def gross_for_director(d)
-  total = 0
-  index = 0
-
-  while index < d[:movies].length do
-    total += d[:movies][index][:worldwide_gross]
-    index += 1
-  end
-
-  total
-end
-
-def list_of_directors(source)
-  # Write this implementation
-end
-
-def total_gross(source)
-  # Write this implementation
-  #
-  # Should use methods:
-  # 1. directors_totals: returns a Hash of { dir_name => gross }
-  # 2. list_of_directors: names provides an Array of directors names (use
-  #
-  # Visit each key (i.e. director name), look up the value in the hash
-  # returned by directors_totals, and add it to a running total. When done,
-  # return the total
-end
-
-
